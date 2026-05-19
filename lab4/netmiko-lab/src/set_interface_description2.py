2d12b1245c17bb2e2f439d823cc1ca5bbcc799ab
Lakshmi <lakshmidivibhavani@gmail.com>
2026-05-19 15:40:07 -0300

    lab4 tasks finished

---
+++ lab4/netmiko-lab/src/set_interface_description2.py
@@ -0,0 +1,15 @@
+from netmiko import Netmiko
+
+device = {
+    "device_type": "cisco_ios",
+    "ip": "192.168.1.101",
+    "username": "student",
+    "password": "Meilab123",
+}
+
+net_connect = Netmiko(**device)
+
+output = net_connect.send_config_from_file("changes.txt")
+print(output)
+
+net_connect.disconnect()
